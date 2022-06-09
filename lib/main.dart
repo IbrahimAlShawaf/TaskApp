cc5c862a93c3dd201bdbe9e308afbd41707949ce
Sana'a Al-ahdal <sanaharun91@gmail.com>
2022-06-09 10:31:34 +0200

    Second Commit for Task App with Sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:tasks_app/db_query.dart';
 import 'package:tasks_app/task_file.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -22,8 +22,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
@@ -32,110 +30,144 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late String? task;
-  DateTime? dateTime= null;
-  List<Task> myTask=[];
-
+  DateTime? dateTime = null;
   TextEditingController controller = TextEditingController();
+  DBQuery dbQuery = DBQuery();
+  late Future<List<Task>> myList;
+
+  @override
+  void initState() {
+    super.initState();
+    myList = dbQuery.getAllTask();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Task App'),
+        title: const Text('Task App'),
       ),
-      body:myTask.isEmpty?Center(child: Text('No Tasks are added yet!',style: TextStyle(fontSize: 22),),):
-      ListView(
-        children: myTask.map((tsk){
-          return ListTile(
-            title: Text("${tsk.taskName}"),
-            subtitle: Text("${tsk.taskDate}"),
-          );
-        } ).toList(),
+      body: FutureBuilder(
+        future: myList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Task>? tasks = snapshot.data as List<Task>?;
+            return ListView.builder(
+                itemCount: tasks!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(tasks[index].t_name.toString(),
+                      style: const TextStyle(fontSize: 20),),
+                    subtitle: Text(
+                      ' ${tasks[index].time.day}/ ${tasks[index].time.month}/'
+                          ' ${tasks[index].time.year}',
+                      style: const TextStyle(fontSize: 17),),);
+                  }
+                  );
+          }
+          else
+            if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+          else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          showModalBottomSheet(context: context,
-              backgroundColor: Colors.transparent,
-              builder: (context){
-            return Container(
-
-                width: 250,
-                height: 400,
-              margin: EdgeInsets.only(left: 8,right:8),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25)
-                    )
-                ),
-              child: Column(
-                children: [
-                 Container(
-                   margin: EdgeInsets.all(16),
-                   child: Row(
-                     children: [
-                       Expanded(flex: 1,child: Text('Task Name:')),
-                       Expanded(flex: 1,child: TextField(
-                         controller: controller,
-                         onSubmitted: (val){
-                           setState(() {
-                             task = controller.text;
-                             controller.clear();
-                           });
-                         },
-                       ),)
-                     ],
-                   ),
-                 ),
-                  Container(
-                    margin: EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        Expanded(flex: 1,child: Text('Task Date')),
-                        Expanded(flex: 1,child: ElevatedButton(
-                          child: Text('Pick Date'),
-                          onPressed: (){
-                            showDatePicker(context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2022),
-                                lastDate: DateTime(2023)).then((value) {
+            showModalBottomSheet(
+                context: context,
+                backgroundColor: Colors.transparent,
+                builder: (context) {
+                  return Container(
+                      width: 250,
+                      height: 400,
+                      margin: EdgeInsets.only(left: 8, right: 8),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25))),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(16),
+                            child: Row(
+                              children: [
+                                Expanded(flex: 1, child: Text('Task Name:')),
+                                Expanded(
+                                  flex: 1,
+                                  child: TextField(
+                                    controller: controller,
+                                    onSubmitted: (val) {
+                                      setState(() {
+                                        task = controller.text;
+                                        controller.clear();
+                                      });
+                                    },
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(16),
+                            child: Row(
+                              children: [
+                                Expanded(flex: 1, child: Text('Task Date')),
+                                Expanded(
+                                    flex: 1,
+                                    child: ElevatedButton(
+                                      child: Text('Pick Date'),
+                                      onPressed: () {
+                                        showDatePicker(
+                                            context: context,
+                                            initialDate: DateTime.now(),
+                                            firstDate: DateTime(2022),
+                                            lastDate: DateTime(2023))
+                                            .then((value) {
+                                          setState(() {
+                                            dateTime = value;
+                                          });
+                                        });
+                                      },
+                                    ))
+                              ],
+                            ),
+                          ),
+                          RawMaterialButton(
+                              child: const Text('Add Task'),
+                              onPressed: () {
+                                if (task != null && dateTime != null) {
                                   setState(() {
-                                    dateTime = value;
-                                  });
-                            });
-                          },
-                        )
-                        )
-                      ],
-                    ),
-                  ),
-                  RawMaterialButton(
-                      child: Text('Add Task'),
-                      onPressed: (){
-                        if(task!=null && dateTime!=null)
-                          {
-                            setState(() {
-                              myTask.add(Task(taskName: task!,taskDate: dateTime!));
-                              task= null;
-                              dateTime=null;
-                            });
-                            Navigator.pop(context);
-                          }
-                        else{
-                          return;
-                        }
+                                    taskTime time = taskTime(day: dateTime!.day, dayName: dateTime!.day.toString(), month: dateTime!.month,
+                                        year: dateTime!.year);
 
-                      })
-                ],
-              )
-            );
-          });
+                                    Task t = Task(t_name: task!, time: time);
+                                    dbQuery.SaveTask(t);
+                                    task = null;
+                                    dateTime = null;
+                                    myList = dbQuery.getAllTask();
+                                  });
+                                  Navigator.pop(context);
+                                } else {
+                                  return;
+                                }
+                              })
+                        ],
+                      ));
+                });
         },
         child: const Icon(Icons.add),
       ),
     );
   }
+
 
 
 }
--- lib/task_file.dart
+++ lib/task_file.dart
@@ -1,40 +1,32 @@
 
-class Task{
-  final String taskName;
-  final DateTime taskDate;
-  Task({required this.taskName, required this.taskDate});
+class Task {
+   int? t_id;
+   final String t_name;
+   final taskTime time;
+
+  Task({ required this.t_name, required this.time});
+
+// Convert a Task into Map
+  Map<String ,dynamic> toMap(){
+    var map = new Map<String, dynamic>();
+    map['t_id']=t_id;
+    map['t_name']= t_name;
+    map['t_day'] = time.day;
+    map['t_month']= time.month;
+    map['t_year']=time.year;
+    map['dayName']=time.dayName;
+    return map;
+  }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class taskTime{
+  final int day;
+  final int month;
+  final int year;
+  final String dayName;
+  taskTime({required this.day,required this.dayName ,required this.month , required this.year});
+}
 
 
 
